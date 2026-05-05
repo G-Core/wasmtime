1330920c0eab1b61dcc78e7ac7574a5a3326a30b
Ruslan Pislari <ruslan.pislari@gcore.com>
2026-05-05 10:26:27 +0300

    fix: remove HOST from DEFAULT_FORBIDDEN_HEADERS list

--- crates/wasi-http/src/types.rs
+++ crates/wasi-http/src/types.rs
@@ -321,7 +321,7 @@ impl<T: WasiHttpView> WasiHttpView for WasiHttpImpl<T> {
 
 /// Set of [http::header::HeaderName], that are forbidden by default
 /// for requests and responses originating in the guest.
-pub const DEFAULT_FORBIDDEN_HEADERS: [http::header::HeaderName; 9] = [
+pub const DEFAULT_FORBIDDEN_HEADERS: [http::header::HeaderName; 8] = [
     hyper::header::CONNECTION,
     HeaderName::from_static("keep-alive"),
     hyper::header::PROXY_AUTHENTICATE,
@@ -329,7 +329,6 @@ pub const DEFAULT_FORBIDDEN_HEADERS: [http::header::HeaderName; 9] = [
     HeaderName::from_static("proxy-connection"),
     hyper::header::TRANSFER_ENCODING,
     hyper::header::UPGRADE,
-    hyper::header::HOST,
     HeaderName::from_static("http2-settings"),
 ];
 
